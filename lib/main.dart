71257ba58acaaf7f07eb25469422a5ea1bf420be
John-Thailand <hamoban69@gmail.com>
2022-05-02 15:24:58 +0900

    Firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
@@ -7,7 +8,12 @@ import 'package:getx_todo_list/app/modules/home/view.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  // init firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // start the storage drive
   await GetStorage.init();
+  // async version of Ger.put()
   await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
